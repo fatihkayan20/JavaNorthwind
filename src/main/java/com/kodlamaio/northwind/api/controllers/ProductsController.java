6f4d59ebea413a07c0bbb05089a8db4abea84065
fatih <fatih@FATIH>
2021-05-09 16:06:33 +0300

    first commit

---
+++ src/main/java/com/kodlamaio/northwind/api/controllers/ProductsController.java
@@ -0,0 +1,28 @@
+package com.kodlamaio.northwind.api.controllers;
+
+import com.kodlamaio.northwind.business.abstracts.ProductService;
+import com.kodlamaio.northwind.entities.concretes.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products/")
+public class ProductsController {
+
+    private ProductService productService;
+
+    @Autowired
+    public ProductsController(ProductService productService) {
+        this.productService = productService;
+    }
+
+
+    @GetMapping("/getall")
+    public List<Product> getAll(){
+        return this.productService.getAll();
+    }
+}
